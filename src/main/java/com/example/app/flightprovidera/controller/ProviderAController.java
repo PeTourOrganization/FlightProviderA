2c3290caae8b21d3f915393a1761592dc3fbbff4
aa-cmd <goetherwerther94@gmail.com>
2026-02-19 22:34:26 +0300

    controller added

--- src/main/java/com/example/app/flightprovidera/controller/ProviderAController.java
+++ src/main/java/com/example/app/flightprovidera/controller/ProviderAController.java
@@ -1,9 +1,15 @@
 package com.example.app.flightprovidera.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/providerA")
 public class ProviderAController {
+
+    @GetMapping("/flightsAvailable")
+    public String getFlightsAvailable() {
+        return "Flights Available";
+    }
 }
