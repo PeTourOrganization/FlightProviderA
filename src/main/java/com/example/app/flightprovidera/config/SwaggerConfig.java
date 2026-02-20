06ab2fddcdd01af63894e25f272102dfb5463e25
aa-cmd <goetherwerther94@gmail.com>
2026-02-20 16:47:36 +0300

    some unnecessary configurations commented

--- src/main/java/com/example/app/flightprovidera/config/SwaggerConfig.java
+++ src/main/java/com/example/app/flightprovidera/config/SwaggerConfig.java
@@ -1,21 +1,21 @@
-package com.example.app.flightprovidera.config;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@ConditionalOnWebApplication
-public class SwaggerConfig implements WebMvcConfigurer {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().components(new Components());
-    }
-
-
-}
-
+//package com.example.app.flightprovidera.config;
+//
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@ConditionalOnWebApplication
+//public class SwaggerConfig implements WebMvcConfigurer {
+//
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI().components(new Components());
+//    }
+//
+//
+//}
+//
